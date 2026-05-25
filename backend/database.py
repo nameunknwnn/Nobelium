47f144a070663cb58539b431f58703b2103f19eb
nameunknwnn <adirawat2016@gmail.com>
2026-05-26 00:55:24 +0530

    added db ssl thing

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,10 @@ from config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
 )
 
 
